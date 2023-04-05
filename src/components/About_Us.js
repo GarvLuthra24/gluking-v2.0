10efd49110bc327ce44d0e438af12a688245b9aa
Garv <99045108+GarvLuthra24@users.noreply.github.com>
2023-04-06 04:40:11 +0530

    about-us complete

--- src/components/About_Us.js
+++ src/components/About_Us.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Tree from './images/tree.png'
 
 const About_Us = () => {
   return (
@@ -33,17 +34,25 @@ const About_Us = () => {
  
 
         </div>
-        <blockquote class=" text-base italic font-semibold text-white  px-16 py-10 bg-black">
+        <blockquote class=" text-base italic font-semibold text-white  px-16 py-12 bg-black">
     <p>"Since 2017,We are actively investing and innovating to make sure our silicone materials continue to bring their unique performance and sustainability benefits to society."</p>
     <div className=' w-full flex flex-row  justify-end mt-3' >-Team Gluking</div>
 
     
 </blockquote>
 
+<div className=' text-6xl flex flex-row justify-center px-5 py-4 my-5'> Our Core Values</div>
+
     <div className='  flex flex-row flex-wrap justify-center'>
     <div class="max-w-sm rounded overflow-hidden shadow-lg mx-2">
-  {/* <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
+
+ 
+  
   <div class="px-6 py-4">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" mx-auto w-24  h-24">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18" />
+</svg>
+
     <div class="font-bold text-xl mb-2">Smart</div>
     <p class="text-gray-700 text-base">
     Means that we deliver what we promise-and add value that goes beyond what is expected. We achieve Excellence through Innovation, Learning and Agility.
@@ -63,6 +72,8 @@ const About_Us = () => {
 <div class="max-w-sm rounded overflow-hidden shadow-lg mx-2">
   {/* <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
   <div class="px-6 py-4">
+<svg id="Layer_1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" className=' w-24 h-24 m-auto' viewBox="0 0 122.88 105.6"><title>discussion</title><path d="M26.89,0h44a8,8,0,0,1,7.94,7.93v22.8a8,8,0,0,1-7.94,7.94H40.61L28.56,49a1.32,1.32,0,0,1-1.88-.15,1.26,1.26,0,0,1-.32-.95L27,38.66h-.11A8,8,0,0,1,19,30.73V7.93A8,8,0,0,1,26.89,0ZM0,105.6c1-13,3.45-8.13,14.53-15.06,3.06,7.94,17.74,8.47,21,0C45.06,96.65,49.84,92,49.77,105.6ZM18,89.92a4.86,4.86,0,0,1,.49-1.72,6.94,6.94,0,0,1-2.74-5.08h-.15a2,2,0,0,1-1-.26,2.67,2.67,0,0,1-1.09-1.33c-.5-1.15-.9-4.2.37-5.07l-.24-.16,0-.34c-.05-.61-.06-1.35-.07-2.13,0-2.86-.1-6.33-2.41-7l-1-.3.66-.8a37.74,37.74,0,0,1,5.76-5.86,14.82,14.82,0,0,1,6.68-3.24,8,8,0,0,1,6.49,1.82,11.52,11.52,0,0,1,1.74,1.75,7.34,7.34,0,0,1,5.2,3,10.42,10.42,0,0,1,1.69,3.42,11.52,11.52,0,0,1,.46,3.87,9.29,9.29,0,0,1-2.7,6.28,1.84,1.84,0,0,1,.84.22,2.09,2.09,0,0,1,.74,2.56c-.25.78-.57,1.69-.87,2.45-.36,1-.89,1.22-1.92,1.11,0,2.54-1.22,3.79-2.8,5.29l.35,1.25c-1,4.84-11.59,5.37-14.46.27Zm29.83-4.46A8.28,8.28,0,0,1,44,83c2.58-1,3.77-3.58,3.94-8.14.12-3.38-.58-5.94.57-9.28,2.29-6.62,10.75-8.88,15.46-5,3.7-.39,7.43,1.52,8.23,7.21.59,4.26-.67,8,.67,11.89A6.1,6.1,0,0,0,76,83.44a9.15,9.15,0,0,1-4.22,2,42,42,0,0,1-6.64.74v2l2.32,3.7L60,97.74,52.51,92l1.65-3.55V86.25a29.34,29.34,0,0,1-6.35-.79ZM71.7,95.07a18.08,18.08,0,0,0-3.46-5.37c2.81,1.08,12.19,3.43,13.64,6.15,1.64,3.07,1.4,6.48,2.27,9.75H53.65c0-5.26-.55-8.43-1.72-10.51l7.84,6.27L68.14,95l3.56.08ZM46.76,91.19A50.14,50.14,0,0,0,51.7,89.7a14.64,14.64,0,0,0-2.16,2.85,13.83,13.83,0,0,0-2.78-1.36Zm44.94-.34a4.38,4.38,0,0,1,.45-1.6,6.56,6.56,0,0,1-2.57-4.78h-.14a2,2,0,0,1-.94-.25,2.55,2.55,0,0,1-1-1.24c-.48-1.09-.85-4,.34-4.78l-.22-.14,0-.32c0-.58-.06-1.27-.07-2,0-2.69-.1-6-2.26-6.6l-.93-.29.61-.75a35.38,35.38,0,0,1,5.42-5.5,13.7,13.7,0,0,1,6.28-3.05,7.52,7.52,0,0,1,6.1,1.7,11.64,11.64,0,0,1,1.64,1.65,6.93,6.93,0,0,1,4.89,2.85A9.9,9.9,0,0,1,110.84,69a10.88,10.88,0,0,1,.42,3.64,8.72,8.72,0,0,1-2.53,5.9,1.78,1.78,0,0,1,.79.2,2,2,0,0,1,.69,2.41c-.23.74-.53,1.59-.81,2.31-.34,1-.84,1.14-1.8,1,0,2.39-1.16,3.56-2.65,5l.34,1.17c-.94,4.55-10.9,5.05-13.59.25Zm-6.79,2.54a35.37,35.37,0,0,0,3.54-2c2.88,7.46,16.68,8,19.71,0,9,5.75,14.78,1.4,14.72,14.16H88a3.89,3.89,0,0,0-.13-1,27,27,0,0,1-.58-3.2,20.48,20.48,0,0,0-2-7.36,5.73,5.73,0,0,0-.39-.64ZM82.74,16.73a7.81,7.81,0,0,1,6.31,7.64v19a7.78,7.78,0,0,1-7.77,7.77H81l.65,9.43h0a1.17,1.17,0,0,1-1.93,1L67.63,50.81H35.11l7.84-8H76.17a6.62,6.62,0,0,0,6.59-6.6v-19c0-.18,0-.35,0-.53ZM30.8,24a1.34,1.34,0,0,1,0-2.68H61.39a1.34,1.34,0,0,1,0,2.68Zm0-9.18a1.34,1.34,0,0,1,0-2.67H67.24a1.34,1.34,0,1,1,0,2.67ZM70.91,2.68h-44a5.28,5.28,0,0,0-5.26,5.25v22.8A5.29,5.29,0,0,0,26.89,36h1.63a1.34,1.34,0,0,1,1.24,1.42l-.52,7.5,9.94-8.54a1.32,1.32,0,0,1,.94-.39H70.91a5.29,5.29,0,0,0,5.26-5.26V7.93a5.28,5.28,0,0,0-5.26-5.25Z"/></svg>
+
     <div class="font-bold text-xl mb-2 ">Stable</div>
     <p class="text-gray-700 text-base">
     The best solutions come from working together with both colleagues and clients. Effective teamwork demands strong Relationships, Respect and Sharing.
@@ -80,7 +91,12 @@ const About_Us = () => {
 
 <div class="max-w-sm rounded overflow-hidden shadow-lg mx-2">
   {/* <img class="w-full" src="/img/card-top.jpg" alt="Sunset in the mountains"> */}
+  
   <div class="px-6 py-4">
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-24 h-24 m-auto">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z" />
+</svg>
+    
     <div class="font-bold text-xl mb-2">Strong</div>
     <p class="text-gray-700 text-base">
     Means leading with clients and leading with people. Leadership in our business is defined by courage, personal integrity, and having a vision which inspires and motivates others.
@@ -95,6 +111,18 @@ const About_Us = () => {
             
         
     </div>
+
+
+    <div  class=" my-12 mx-auto flex flex-col items-center bg-green-900  border border-gray-200 rounded-lg shadow md:flex-row md:max-w-6xl  dark:border-gray-700  ">
+    {/* <img class="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src="/docs/images/blog/image-4.jpg" alt=""> */}
+    <img src={Tree} className=' w-40 rounded-lg'></img>
+    <div class="flex flex-col justify-between p-4 leading-normal">
+        <div class="mb-2  font-bold tracking-tight text-white dark:text-white text-3xl"> 
+
+Sustainability at Gluking </div>
+        <p class="mb-3 font-normal text-white">Developing tomorrow’s solutions – this is what we have set out to achieve. These solutions help us to live more sustainably, to conserve resources, and to use alternative energy sources so that we can address the climate crisis together.</p>
+    </div>
+</div>
     
 
     
