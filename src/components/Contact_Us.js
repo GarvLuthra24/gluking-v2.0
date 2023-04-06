2a01563b0a9fdcc1a43eb3c90e8f197ac425f5fc
Garv <99045108+GarvLuthra24@users.noreply.github.com>
2023-04-07 01:00:58 +0530

    fix 3

--- src/components/Contact_Us.js
+++ src/components/Contact_Us.js
@@ -11,20 +11,11 @@ const Contact_Us = () => {
     <div class="flex flex-col justify-between p-4 leading-normal ml-5">
         <div class="mb-2 py-5 text-3xl  font-bold tracking-tight text-white dark:text-white "> 
 
-        Get In Touch With Us </div>
+        Locate Us</div>
         <div class="mb-3 font-normal text-white text-lg">
           <ul>
-            <li className=' mb-3 flex flex-row flex-wrap'>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-3">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-            </svg>
-            <div>
-            9211397395 , 011-47026009
-            </div>
 
-            </li>
-
-            <li className=' mb-3 flex flex-row flex-wrap'>
+          <li className=' mb-3 flex flex-row flex-wrap'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-3">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
             </svg>
@@ -38,6 +29,18 @@ const Contact_Us = () => {
             </div>
 
             </li>
+
+            <li className=' mb-3 flex flex-row flex-wrap'>
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mx-3">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+            </svg>
+            <div>
+            9211397395 , 011-47026009
+            </div>
+
+            </li>
+
+
 
 
 
